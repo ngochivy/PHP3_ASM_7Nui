550554e34ed31bba84dc0d3f475b54b077966405
Group 6 <nam219021@gmail.com>
2025-03-31 15:32:19 +0700

    trang-chu-31-3

---
+++ resources/views/client/auth/account.blade.php
@@ -0,0 +1,39 @@
+@extends('client.layouts.auth')
+@push('stylesheet')
+<!--== Favicon ==-->
+<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
+
+<!--== Google Fonts ==-->
+<link href="https://fonts.googleapis.com/css?family=Fredoka+One:400" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!--== Bootstrap CSS ==-->
+<link href="{{asset ('assets/css/bootstrap.min.css')}}" rel="stylesheet"/>
+<!--== Font-awesome Icons CSS ==-->
+<link href="{{asset ('assets/css/font-awesome.min.css')}}" rel="stylesheet"/>
+<!--== Pe Icon 7 Min Icons CSS ==-->
+<link href="{{asset ('assets/css/pe-icon-7-stroke.min.css')}}" rel="stylesheet"/>
+<!--== Ionicons CSS ==-->
+<link href="{{asset ('assets/css/ionicons.css')}}" rel="stylesheet"/>
+<!--== Animate CSS ==-->
+<link href="{{asset ('assets/css/animate.css')}}" rel="stylesheet"/>
+<!--== Aos CSS ==-->
+<link href="{{asset ('assets/css/aos.css')}}" rel="stylesheet"/>
+<!--== FancyBox CSS ==-->
+<link href="{{asset ('assets/css/jquery.fancybox.min.css')}}" rel="stylesheet"/>
+<!--== Slicknav CSS ==-->
+<link href="{{asset ('assets/css/slicknav.css')}}" rel="stylesheet"/>
+<!--== Swiper CSS ==-->
+<link href="{{asset ('assets/css/swiper.min.css')}}" rel="stylesheet"/>
+<!--== Slick CSS ==-->
+<link href="{{asset ('assets/css/slick.css')}}" rel="stylesheet"/>
+<!--== Rangeslider CSS ==-->
+<link href="{{asset ('assets/css/rangeslider.css')}}" rel="stylesheet"/>
+<!--== Main Style CSS ==-->
+<link href="{{asset ('assets/css/style.css')}}" rel="stylesheet" />
+
+@endpush
+
+@section('content')
+
+@endsection
