50c3c04a15005574e0e8ba20508661c1ab283a05
NhutLinh-1007 <linhlove00b@gmail.com>
2025-03-31 17:21:05 +0700

    Client for Checkout, About, Contact

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
