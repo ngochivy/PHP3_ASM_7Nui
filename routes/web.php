06fa072102b5dd91c7a4f914fa92d6ee32dfb7a3
Group 6 <nam219021@gmail.com>
2025-04-01 00:43:59 +0700

    trang-chu

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,12 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/', [HomeController::class, "index"]);
