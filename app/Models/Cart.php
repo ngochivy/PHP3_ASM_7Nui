da76493cfa86d42dadefc6725c68d50660583bb5
Group 6 <nam219021@gmail.com>
2025-04-01 00:24:11 +0700

    database-update

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Cart extends Model
+class Category extends Model
 {
     //
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
     protected $fillable = [
