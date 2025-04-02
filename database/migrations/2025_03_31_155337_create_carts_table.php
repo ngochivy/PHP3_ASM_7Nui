2328d30ee02b3b56be25a206d0e376d5354847bb
Group 6 <nam219021@gmail.com>
2025-04-02 19:09:18 +0700

    update-migrate-cart

--- database/migrations/2025_03_31_155337_create_carts_table.php
+++ database/migrations/2025_03_31_155337_create_carts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->integer('qty');
             
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             
